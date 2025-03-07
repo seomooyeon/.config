480f6cc4368cd44e40f6934def4d9e8224473945
Mooyeon Seo <seomooyeon@gmail.com>
2025-03-07 22:34:38 +0900

    fix: resolve type from exa to eza

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install packages with Homebrew
-brew install yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick zellij exa
+brew install yazi ffmpeg p7zip jq poppler fd ripgrep fzf zoxide imagemagick zellij eza
 
 # Install zshrc
 cp .zshrc ~/.zshrc
