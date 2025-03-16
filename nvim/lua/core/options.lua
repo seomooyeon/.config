49609451271ccb0eb2e0f2c882903a1fe8c8711c
Mooyeon Seo <seomooyeon@gmail.com>
2025-03-16 21:44:10 +0900

    feat: move config to core

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -43,12 +43,6 @@ opt.swapfile = true
 
 vim.opt.swapfile = false
 
--- kickstart
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
