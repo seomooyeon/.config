ff5b0ea1580d67d8f10811093970a53611dd2d8f
mooyeon-seo <seomooyeon@gmail.com>
2025-03-16 19:03:23 +0900

    feat: remove unused keymaps in substitute

--- nvim/lua/plugins/substitute.lua
+++ nvim/lua/plugins/substitute.lua
@@ -8,10 +8,6 @@ return {
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
-
-		vim.keymap.set("n", "s", substitute.operator, { desc = "Substitute with motion" })
-		vim.keymap.set("n", "ss", substitute.line, { desc = "Substitute line" })
-		vim.keymap.set("n", "S", substitute.eol, { desc = "Substitute to end of line" })
 		vim.keymap.set("x", "s", substitute.visual, { desc = "Substitute in visual mode" })
 	end,
 }
