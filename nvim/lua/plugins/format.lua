8bdedd2decd2e76cbdcbf384a0f916e14c49a203
mooyeon-seo <seomooyeon@gmail.com>
2025-03-17 22:27:56 +0900

    feat: remove support for unused languages

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,20 +1,16 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
 		conform.setup({
 			formatters_by_ft = {
 				javascript = { "prettier" },
-				typescript = { "prettier" },
-				typescriptreact = { "prettier" },
 				css = { "prettier" },
 				scss = { "prettier" },
 				html = { "prettier" },
 				json = { "prettier" },
-				lua = { "stylua" },
-				python = { "isort", "black" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
