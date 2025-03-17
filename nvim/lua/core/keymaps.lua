2dea7d7a2cbc9c2d5b8e5e13a65a5d10ff663ca3
mooyeon-seo <seomooyeon@gmail.com>
2025-03-17 22:38:09 +0900

    chore: remove unused nvim keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -4,49 +4,44 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 -- General keymaps
-keymap.set("n", "<leader>wq", ":wq<CR>") -- save and quit
-keymap.set("n", "<leader>qq", ":q!<CR>") -- quit without saving
-keymap.set("n", "<leader>ww", ":w<CR>") -- save
+keymap.set("n", "<leader>wq", ":wq<CR>")       -- save and quit
+keymap.set("n", "<leader>qq", ":q!<CR>")       -- quit without saving
+keymap.set("n", "<leader>ww", ":w<CR>")        -- save
 keymap.set("n", "gx", ":!open <c-r><c-a><CR>") -- open URL under cursor
 
 -- Split window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>sv", "<C-w>v")     -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s")     -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=")     -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close split window
-keymap.set("n", "<leader>sj", "<C-w>-") -- make split window height shorter
-keymap.set("n", "<leader>sk", "<C-w>+") -- make split windows height taller
-keymap.set("n", "<leader>sl", "<C-w>>5") -- make split windows width bigger
-keymap.set("n", "<leader>sh", "<C-w><5") -- make split windows width smaller
+keymap.set("n", "<leader>sj", "<C-w>-")     -- make split window height shorter
+keymap.set("n", "<leader>sk", "<C-w>+")     -- make split windows height taller
+keymap.set("n", "<leader>sl", "<C-w>>5")    -- make split windows width bigger
+keymap.set("n", "<leader>sh", "<C-w><5")    -- make split windows width smaller
 
 -- Tab management
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
+keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open a new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close a tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") -- previous tab
+keymap.set("n", "<leader>tn", ":tabn<CR>")     -- next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>")     -- previous tab
 
 -- Diff keymaps
-keymap.set("n", "<leader>cc", ":diffput<CR>") -- put diff from current to other during diff
+keymap.set("n", "<leader>cc", ":diffput<CR>")   -- put diff from current to other during diff
 keymap.set("n", "<leader>cj", ":diffget 1<CR>") -- get diff from left (local) during merge
 keymap.set("n", "<leader>ck", ":diffget 3<CR>") -- get diff from right (remote) during merge
-keymap.set("n", "<leader>cn", "]c") -- next diff hunk
-keymap.set("n", "<leader>cp", "[c") -- previous diff hunk
+keymap.set("n", "<leader>cn", "]c")             -- next diff hunk
+keymap.set("n", "<leader>cp", "[c")             -- previous diff hunk
 
 -- Quickfix keymaps
-keymap.set("n", "<leader>qo", ":copen<CR>") -- open quickfix list
+keymap.set("n", "<leader>qo", ":copen<CR>")  -- open quickfix list
 keymap.set("n", "<leader>qf", ":cfirst<CR>") -- jump to first quickfix list item
-keymap.set("n", "<leader>qn", ":cnext<CR>") -- jump to next quickfix list item
-keymap.set("n", "<leader>qp", ":cprev<CR>") -- jump to prev quickfix list item
-keymap.set("n", "<leader>ql", ":clast<CR>") -- jump to last quickfix list item
+keymap.set("n", "<leader>qn", ":cnext<CR>")  -- jump to next quickfix list item
+keymap.set("n", "<leader>qp", ":cprev<CR>")  -- jump to prev quickfix list item
+keymap.set("n", "<leader>ql", ":clast<CR>")  -- jump to last quickfix list item
 keymap.set("n", "<leader>qc", ":cclose<CR>") -- close quickfix list
 
 -- Vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle maximize tab
-
--- Nvim-tree
-keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>") -- toggle file explorer
-keymap.set("n", "<leader>er", ":NvimTreeFocus<CR>") -- toggle focus to file explorer
-keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file explorer
 
 -- -- Telescope
 -- keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, {}) -- fuzzy find files in project
@@ -71,31 +66,31 @@ keymap.set("n", "<leader>gb", ":GitBlameToggle<CR>") -- toggle git blame
 keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
 keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
 keymap.set("n", "<leader>h1", function()
-	require("harpoon.ui").nav_file(1)
+  require("harpoon.ui").nav_file(1)
 end)
 keymap.set("n", "<leader>h2", function()
-	require("harpoon.ui").nav_file(2)
+  require("harpoon.ui").nav_file(2)
 end)
 keymap.set("n", "<leader>h3", function()
-	require("harpoon.ui").nav_file(3)
+  require("harpoon.ui").nav_file(3)
 end)
 keymap.set("n", "<leader>h4", function()
-	require("harpoon.ui").nav_file(4)
+  require("harpoon.ui").nav_file(4)
 end)
 keymap.set("n", "<leader>h5", function()
-	require("harpoon.ui").nav_file(5)
+  require("harpoon.ui").nav_file(5)
 end)
 keymap.set("n", "<leader>h6", function()
-	require("harpoon.ui").nav_file(6)
+  require("harpoon.ui").nav_file(6)
 end)
 keymap.set("n", "<leader>h7", function()
-	require("harpoon.ui").nav_file(7)
+  require("harpoon.ui").nav_file(7)
 end)
 keymap.set("n", "<leader>h8", function()
-	require("harpoon.ui").nav_file(8)
+  require("harpoon.ui").nav_file(8)
 end)
 keymap.set("n", "<leader>h9", function()
-	require("harpoon.ui").nav_file(9)
+  require("harpoon.ui").nav_file(9)
 end)
 
 -- Vim REST Console
@@ -121,27 +116,27 @@ keymap.set("n", "<leader>xr", ":call VrcQuery()<CR>") -- Run REST query
 
 -- Filetype-specific keymaps (these can be done in the ftplugin directory instead if you prefer)
 keymap.set("n", "<leader>go", function()
-	if vim.bo.filetype == "java" then
-		require("jdtls").organize_imports()
-	end
+  if vim.bo.filetype == "java" then
+    require("jdtls").organize_imports()
+  end
 end)
 
 keymap.set("n", "<leader>gu", function()
-	if vim.bo.filetype == "java" then
-		require("jdtls").update_projects_config()
-	end
+  if vim.bo.filetype == "java" then
+    require("jdtls").update_projects_config()
+  end
 end)
 
 keymap.set("n", "<leader>tc", function()
-	if vim.bo.filetype == "java" then
-		require("jdtls").test_class()
-	end
+  if vim.bo.filetype == "java" then
+    require("jdtls").test_class()
+  end
 end)
 
 keymap.set("n", "<leader>tm", function()
-	if vim.bo.filetype == "java" then
-		require("jdtls").test_nearest_method()
-	end
+  if vim.bo.filetype == "java" then
+    require("jdtls").test_nearest_method()
+  end
 end)
 
 -- Debugging
@@ -155,24 +150,24 @@ keymap.set("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>")
 keymap.set("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>")
 keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>")
 keymap.set("n", "<leader>dd", function()
-	require("dap").disconnect()
-	require("dapui").close()
+  require("dap").disconnect()
+  require("dapui").close()
 end)
 keymap.set("n", "<leader>dt", function()
-	require("dap").terminate()
-	require("dapui").close()
+  require("dap").terminate()
+  require("dapui").close()
 end)
 keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>")
 keymap.set("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>")
 keymap.set("n", "<leader>di", function()
-	require("dap.ui.widgets").hover()
+  require("dap.ui.widgets").hover()
 end)
 keymap.set("n", "<leader>d?", function()
-	local widgets = require("dap.ui.widgets")
-	widgets.centered_float(widgets.scopes)
+  local widgets = require("dap.ui.widgets")
+  widgets.centered_float(widgets.scopes)
 end)
 keymap.set("n", "<leader>df", "<cmd>Telescope dap frames<cr>")
 keymap.set("n", "<leader>dh", "<cmd>Telescope dap commands<cr>")
 keymap.set("n", "<leader>de", function()
-	require("telescope.builtin").diagnostics({ default_text = ":E:" })
+  require("telescope.builtin").diagnostics({ default_text = ":E:" })
 end)
