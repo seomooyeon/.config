b271092f2b41425a501cae2e0e2c826e6a28ccec
mooyeon-seo <seomooyeon@gmail.com>
2025-03-17 23:25:48 +0900

    feat: add stylua again as a formatter for lua

--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,22 +1,23 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 3000,
-			},
-		})
-	end,
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        css = { "prettier" },
+        scss = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 3000,
+      },
+    })
+  end,
 }
