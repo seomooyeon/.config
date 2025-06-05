015dd90bd015430b91efeb41e834ae5dc5fc4b2f
Mooyeon Seo <seomooyeon@gmail.com>
2025-06-04 20:38:31 -0400

    remove java stuff

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -4,71 +4,71 @@ vim.g.mapleader = " "
 local keymap = vim.keymap
 
 -- General keymaps
-keymap.set("n", "<leader>wq", ":wq<CR>")       -- save and quit
-keymap.set("n", "<leader>qq", ":q!<CR>")       -- quit without saving
-keymap.set("n", "<leader>ww", ":w<CR>")        -- save
+keymap.set("n", "<leader>wq", ":wq<CR>") -- save and quit
+keymap.set("n", "<leader>qq", ":q!<CR>") -- quit without saving
+keymap.set("n", "<leader>ww", ":w<CR>") -- save
 keymap.set("n", "gx", ":!open <c-r><c-a><CR>") -- open URL under cursor
 
 -- Split window management
-keymap.set("n", "<leader>sv", "<C-w>v")     -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s")     -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=")     -- make split windows equal width
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close split window
-keymap.set("n", "<leader>sj", "<C-w>-")     -- make split window height shorter
-keymap.set("n", "<leader>sk", "<C-w>+")     -- make split windows height taller
-keymap.set("n", "<leader>sl", "<C-w>>5")    -- make split windows width bigger
-keymap.set("n", "<leader>sh", "<C-w><5")    -- make split windows width smaller
+keymap.set("n", "<leader>sj", "<C-w>-") -- make split window height shorter
+keymap.set("n", "<leader>sk", "<C-w>+") -- make split windows height taller
+keymap.set("n", "<leader>sl", "<C-w>>5") -- make split windows width bigger
+keymap.set("n", "<leader>sh", "<C-w><5") -- make split windows width smaller
 
 -- Tab management
-keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open a new tab
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close a tab
-keymap.set("n", "<leader>tn", ":tabn<CR>")     -- next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>")     -- previous tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- previous tab
 
 -- Diff keymaps
-keymap.set("n", "<leader>cc", ":diffput<CR>")   -- put diff from current to other during diff
+keymap.set("n", "<leader>cc", ":diffput<CR>") -- put diff from current to other during diff
 keymap.set("n", "<leader>cj", ":diffget 1<CR>") -- get diff from left (local) during merge
 keymap.set("n", "<leader>ck", ":diffget 3<CR>") -- get diff from right (remote) during merge
-keymap.set("n", "<leader>cn", "]c")             -- next diff hunk
-keymap.set("n", "<leader>cp", "[c")             -- previous diff hunk
+keymap.set("n", "<leader>cn", "]c") -- next diff hunk
+keymap.set("n", "<leader>cp", "[c") -- previous diff hunk
 
 -- Quickfix keymaps
-keymap.set("n", "<leader>qo", ":copen<CR>")  -- open quickfix list
+keymap.set("n", "<leader>qo", ":copen<CR>") -- open quickfix list
 keymap.set("n", "<leader>qf", ":cfirst<CR>") -- jump to first quickfix list item
-keymap.set("n", "<leader>qn", ":cnext<CR>")  -- jump to next quickfix list item
-keymap.set("n", "<leader>qp", ":cprev<CR>")  -- jump to prev quickfix list item
-keymap.set("n", "<leader>ql", ":clast<CR>")  -- jump to last quickfix list item
+keymap.set("n", "<leader>qn", ":cnext<CR>") -- jump to next quickfix list item
+keymap.set("n", "<leader>qp", ":cprev<CR>") -- jump to prev quickfix list item
+keymap.set("n", "<leader>ql", ":clast<CR>") -- jump to last quickfix list item
 keymap.set("n", "<leader>qc", ":cclose<CR>") -- close quickfix list
 
 -- Harpoon
 keymap.set("n", "<leader>ha", require("harpoon.mark").add_file)
 keymap.set("n", "<leader>hh", require("harpoon.ui").toggle_quick_menu)
 keymap.set("n", "<leader>h1", function()
-  require("harpoon.ui").nav_file(1)
+	require("harpoon.ui").nav_file(1)
 end)
 keymap.set("n", "<leader>h2", function()
-  require("harpoon.ui").nav_file(2)
+	require("harpoon.ui").nav_file(2)
 end)
 keymap.set("n", "<leader>h3", function()
-  require("harpoon.ui").nav_file(3)
+	require("harpoon.ui").nav_file(3)
 end)
 keymap.set("n", "<leader>h4", function()
-  require("harpoon.ui").nav_file(4)
+	require("harpoon.ui").nav_file(4)
 end)
 keymap.set("n", "<leader>h5", function()
-  require("harpoon.ui").nav_file(5)
+	require("harpoon.ui").nav_file(5)
 end)
 keymap.set("n", "<leader>h6", function()
-  require("harpoon.ui").nav_file(6)
+	require("harpoon.ui").nav_file(6)
 end)
 keymap.set("n", "<leader>h7", function()
-  require("harpoon.ui").nav_file(7)
+	require("harpoon.ui").nav_file(7)
 end)
 keymap.set("n", "<leader>h8", function()
-  require("harpoon.ui").nav_file(8)
+	require("harpoon.ui").nav_file(8)
 end)
 keymap.set("n", "<leader>h9", function()
-  require("harpoon.ui").nav_file(9)
+	require("harpoon.ui").nav_file(9)
 end)
 
 -- Vim REST Console
@@ -93,30 +93,6 @@ keymap.set("n", "<leader>xr", ":call VrcQuery()<CR>") -- Run REST query
 -- keymap.set('i', '<C-Space>', '<cmd>lua vim.lsp.buf.completion()<CR>')
 
 -- Filetype-specific keymaps (these can be done in the ftplugin directory instead if you prefer)
-keymap.set("n", "<leader>go", function()
-  if vim.bo.filetype == "java" then
-    require("jdtls").organize_imports()
-  end
-end)
-
-keymap.set("n", "<leader>gu", function()
-  if vim.bo.filetype == "java" then
-    require("jdtls").update_projects_config()
-  end
-end)
-
-keymap.set("n", "<leader>tc", function()
-  if vim.bo.filetype == "java" then
-    require("jdtls").test_class()
-  end
-end)
-
-keymap.set("n", "<leader>tm", function()
-  if vim.bo.filetype == "java" then
-    require("jdtls").test_nearest_method()
-  end
-end)
-
 -- Debugging
 keymap.set("n", "<leader>bb", "<cmd>lua require'dap'.toggle_breakpoint()<cr>")
 keymap.set("n", "<leader>bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>")
@@ -127,21 +103,21 @@ keymap.set("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>")
 keymap.set("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>")
 keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>")
 keymap.set("n", "<leader>dd", function()
-  require("dap").disconnect()
-  require("dapui").close()
+	require("dap").disconnect()
+	require("dapui").close()
 end)
 keymap.set("n", "<leader>dt", function()
-  require("dap").terminate()
-  require("dapui").close()
+	require("dap").terminate()
+	require("dapui").close()
 end)
 keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>")
 keymap.set("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>")
 keymap.set("n", "<leader>di", function()
-  require("dap.ui.widgets").hover()
+	require("dap.ui.widgets").hover()
 end)
 keymap.set("n", "<leader>d?", function()
-  local widgets = require("dap.ui.widgets")
-  widgets.centered_float(widgets.scopes)
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.scopes)
 end)
 -- keymap.set("n", "<leader>ba", "<cmd>lua require('fzf-lua').dap_breakpoints()<cr>")
 -- keymap.set("n", "<leader>df", "<cmd>lua require('fzf-lua').dap_frames()<cr>")
