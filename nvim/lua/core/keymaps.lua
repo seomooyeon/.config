9208bedc01ea52e3432043af7e28759abbba1851
Mooyeon Seo <seomooyeon@gmail.com>
2025-06-05 23:25:42 -0400

    change github copilot shortcut to accept

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -50,3 +50,7 @@ end)
 keymap.set("n", "<leader>h9", function()
 	require("harpoon.ui").nav_file(9)
 end)
+
+-- Copilot
+vim.api.nvim_set_keymap("i", "<M-CR>", 'copilot#Accept("<CR>")', { expr = true, noremap = true, silent = true })
+vim.g.copilot_no_tab_map = true
---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,4 @@
+return {
+	"github/copilot.vim", -- Or the correct plugin name
+	-- Add any specific configurations for Copilot here
+}
