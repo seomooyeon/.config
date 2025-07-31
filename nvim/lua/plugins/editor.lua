9a42c4ae3799ba38da6a9f9a2a639fc02653f385
Mooyeon <seomooyeon@gmail.com>
2025-07-30 22:56:56 -0400

    clean up and remove copilot, and modify dashboard

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -21,33 +21,33 @@ return {
 		"tpope/vim-commentary",
 		event = "VeryLazy",
 	},
-  {
-    	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{
-			"mason-org/mason.nvim",
-			opts = { ensure_installed = { "prettier", "stylua" } },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{
+				"mason-org/mason.nvim",
+				opts = { ensure_installed = { "prettier", "stylua" } },
+			},
 		},
-	},
-	config = function()
-		local conform = require("conform")
+		config = function()
+			local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				lua = { "stylua" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 3000,
-			},
-		})
-	end,
-  }
+			conform.setup({
+				formatters_by_ft = {
+					javascript = { "prettier" },
+					css = { "prettier" },
+					scss = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					lua = { "stylua" },
+				},
+				format_on_save = {
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 3000,
+				},
+			})
+		end,
+	},
 }
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -51,30 +51,16 @@ return {
 			enabled = true,
 			preset = {
 				header = [[
-      ,gg,                                                                
-     i8""8i    I8                                                         
-     `8,,8'    I8                                                         
-      `88'  88888888            gg                                        
-      dP"8,    I8               ""                                        
-     dP' `8a   I8     ,ggggg,   gg     ,gggg,                             
-    dP'   `Yb  I8    dP"  "Y8ggg88    dP"  "Yb                            
-_ ,dP'     I8 ,I8,  i8'    ,8I  88   i8'                                  
-"888,,____,dP,d88b,,d8,   ,d8'_,88,_,d8,_    _                            
-a8P"Y88888P" 8P""Y8P"Y8888P"  8P""Y8P""Y8888PP                            
-     ,gggg,                                                               
-   ,88"""Y8b,                                                        I8   
-  d8"     `Y8                                                        I8   
- d8'   8b  d8                                                  gg 88888888
-,8I    "Y88P'                                                  ""    I8   
-I8'            ,ggggg,    ,ggg,,ggg,,ggg,   ,ggg,,ggg,,ggg,    gg    I8   
-d8            dP"  "Y8ggg,8" "8P" "8P" "8, ,8" "8P" "8P" "8,   88    I8   
-Y8,          i8'    ,8I  I8   8I   8I   8I I8   8I   8I   8I   88   ,I8,  
-`Yba,,_____,,d8,   ,d8' ,dP   8I   8I   Yb,dP   8I   8I   Yb,_,88,_,d88b, 
-  `"Y8888888P"Y8888P"   8P'   8I   8I   `Y8P'   8I   8I   `Y88P""Y88P""Y8 
-                                                                          
-                                                                          
-                                                                          
-                                                                          ]],
+      ,gg,                                     
+     i8""8i    I8                              
+     `8,,8'    I8                              
+      `88'  88888888            gg             
+      dP"8,    I8               ""             
+     dP' `8a   I8     ,ggggg,   gg     ,gggg,  
+    dP'   `Yb  I8    dP"  "Y8ggg88    dP"  "Yb 
+_ ,dP'     I8 ,I8,  i8'    ,8I  88   i8'       
+"888,,____,dP,d88b,,d8,   ,d8'_,88,_,d8,_    _ 
+a8P"Y88888P" 8P""Y8P"Y8888P"  8P""Y8P""Y8888PP ]],
 				keys = {
 					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.picker.files()" },
 					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
@@ -90,12 +76,12 @@ Y8,          i8'    ,8I  I8   8I   8I   8I I8   8I   8I   8I   88   ,I8,
 						desc = "Recent Files",
 						action = ":lua Snacks.dashboard.pick('oldfiles')",
 					},
-					{ 
-            icon = " ", 
-            key = "s", 
-            desc = "Restore Session",
-            section = "session",
-          },
+					{
+						icon = " ",
+						key = "s",
+						desc = "Restore Session",
+						section = "session",
+					},
 					{
 						icon = " ",
 						key = "c",
